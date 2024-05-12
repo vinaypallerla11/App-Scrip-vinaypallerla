6e0862ba37358c71b1ef3b5856e03ffc4cc36d21
vinaypallerla11 <vinaypallerla96@gmail.com>
2024-05-12 18:27:26 +0530

    App Scrip Vinay Pallerla

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/Header'
+import About from './components/About'
+import Skills from './components/Skills'
+import Shop from './components/Shop'
+import Stories from './components/Stories'
+import Contactus from './components/Contactus'
+import Productstabs from './components/Productstabs' 
+import Recommend from './components/Recommend'
+import Products from './components/Products'
+import Footer from './components/Footer'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
-function App() {
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path='/shop' element={<Shop />} />
+          <Route path='/skills' element={<Skills />} />
+          <Route path='/stories' element={<Stories />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/contactus' element={<Contactus />} />
+        </Routes>
+      </BrowserRouter>
+      <Productstabs />
+      <Recommend />
+      <Products/>
+      <Footer/>
     </div>
   );
 }
